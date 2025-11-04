521384e0625fe6df89dd9822275751ade760c697
Aiden Sosa <aidensosa@Aidens-MacBook-Pro.local>
2025-11-04 15:55:12 -0600

    Images

--- Assets/_MyAssets/Scripts/SMasterBulletHolder.cs
+++ Assets/_MyAssets/Scripts/SMasterBulletHolder.cs
@@ -2,11 +2,8 @@ using UnityEngine;
 
 public class SMasterBulletHolder : MonoBehaviour
 {
-    public GameObject mArrow;
-    public GameObject mFireball;
-    public GameObject mBullet;
-
-
-
+    public GameObject mArrow; //0
+    public GameObject mFireball; //1
+    public GameObject mBullet; //2
 
 }
